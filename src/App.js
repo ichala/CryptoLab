5d3249b38211e94a2c34b26bbdf39b26a9fb0a8f
ichala <alijandhd@gmail.com>
2022-02-11 15:45:22 +0100

    installed react-router-dom and start work on home route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Sidebar from "./Components/Sidebar/Sidebar";
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
 function App() {
   return (
     <>
-   
-    <Sidebar/> <div class="wrapper">
-    </div>
+      <Sidebar />
+      <div class="wrapper">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
     </>
   );
 }
