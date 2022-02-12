ea350f424ab41c789519670ba545631ae9cab53f
ichala <alijandhd@gmail.com>
2022-02-12 11:42:41 +0100

    addded news api to redux query

---
+++ src/ApiCalls/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-key': "288132a2aamsha653dc26baa3d3dp187c03jsn050d07fc2aa9",
+  'x-rapidapi-host': "bing-news-search1.p.rapidapi.com",
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: "https://bing-news-search1.p.rapidapi.com/" }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
